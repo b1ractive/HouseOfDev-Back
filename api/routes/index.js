0e46193988c3ce42104fc04b8c617e1d9827b8ea
Santiago <santiango.zanetti@cardenalspionla.com.ar>
2023-10-02 18:57:10 -0300

    Admin routes working and test in Postman

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,9 +3,11 @@ const routes = express.Router();
 const authRoutes = require("./auth.routes.js");
 const userRoutes = require("./user.routes");
 const propertyRoutes = require("./property.routes.js");
+const adminRoutes = require("./admin.routes.js");
 
 routes.use("/auth", authRoutes);
 routes.use("/user", userRoutes);
 routes.use("/property", propertyRoutes);
+routes.use("/admin", adminRoutes);
 
 module.exports = routes;
